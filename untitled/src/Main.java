eb8240183997dae6b03013a32ca30b02db515b6b
GiangLe <giang199603@gmail.com>
2025-03-07 17:10:41 +0700

    Javacore_Lab_05

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,13 +1,22 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int n;
-        Scanner sc = new Scanner(System.in);
-        n=sc.nextInt();
-        String[] country=new String[n];
-        System.out.println(country[0]);
+//        int n;
+//        Scanner sc = new Scanner(System.in);
+//        n=sc.nextInt();
+//        String[] country=new String[n];
+//        System.out.println(country[0]);
+
+        ArrayList cars = new ArrayList();
+        //ArrayList<String> cars2 = new ArrayList<String>();//Generic
+        cars.add(2);
+        cars.add("Audi");
+        cars.add("Honda");
+        cars.add(true);
+        System.out.println(cars);
+        System.out.println(cars.size());
     }
 }
